10146fd4bedd4200d4358f2e15a17e657252627b
910225069jianfa <115801467+910225069jianfa@users.noreply.github.com>
2022-11-14 18:10:08 +0800

    Add files via upload

---
+++ 递归_汉诺塔问题.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int cnt = 0;
+void move(int, char, char, char);
+
+int main()
+{
+    int n;
+    printf("请输入盘子的数量");
+    scanf("%d", &n);
+    
+    char a = 'A';
+    char b = 'B';
+    char c = 'C';
+
+    move(n, a, b, c);
+
+    printf("一共移动了%d次\n", cnt); 
+
+    return 0;
+}
+
+void move(int n, char a, char b, char c)
+{
+    if (n == 1)
+        printf("第%d次: %c -> %c\n", ++cnt, a, b);
+    else
+    {
+        move(n - 1, a, c, b); //把n - 1个盘子从A移到C
+        printf("第%d次: %c -> %c\n", ++cnt, a, b); //把最下面的盘子从A移到B
+        move(n - 1, c, b, a); //再把n - 1个盘中从C移到B
+    }
+}
